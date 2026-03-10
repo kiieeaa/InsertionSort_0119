3b78464cf90379edb459806aac1edf83fdbd25af
kiieeaa <kiyaadinda5@gmail.com>
2026-03-10 10:23:14 +0700

    memberikan pesan error jika banyaknya elemen pada array lebih dari 20

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -3,3 +3,16 @@ using namespace std;
 
 int arr[20];        //membuat array dengan panjang data 20
 int n;              //membuat variable inputan n
+
+void input() { //procedure input
+    while (true) {
+        cout << "masukan jumlah data pada array: "; //meminta user untuk memasukkan banyaknya elemen pada array
+        cin >> n; //menerima inputan banyaknya elemen pada array
+
+        if (n <= 20) { //jika banyaknya elemen pada array kurang dari atau sama dengan 20
+            break; //keluar dari loop
+        }
+        else {
+            cout << "\nArray tidak boleh lebih dari 20\n." << endl; //memberikan pesan error jika banyaknya elemen pada array lebih dari 20
+        }
+    }
